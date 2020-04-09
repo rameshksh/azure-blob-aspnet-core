1bebaca3d277db3cedb0a9391b1d850283f1e6f7
rameshksh <ramesh.sharma12@hotmail.com>
2020-04-09 18:59:35 +0530

    blob demo

---
+++ AzureBlobDemo/AzureBlobDemo/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AzureBlobDemo.Models;
+using System.IO;
+using Microsoft.Azure.Storage.Blob;
+using AzureBlobDemo.Services;
+
+namespace AzureBlobDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string id)
+        {
+            foreach (var file in Request.Form.Files)
+            {
+                // Container Name - picture  
+                BlobManager BlobManagerObj = new BlobManager("photos");
+                string FileAbsoluteUri = BlobManagerObj.UploadFile(file);
+
+                return RedirectToAction("Get");
+            }
+
+            return View();
+           
+        }
+
+        public ActionResult Get()
+        {
+            // Container Name - picture  
+            BlobManager BlobManagerObj = new BlobManager("photos");
+            List<string> fileList = BlobManagerObj.BlobList();
+            return View("LIST", fileList);
+        }
+
+        public ActionResult Delete(string uri)
+        {
+            // Container Name - picture  
+            BlobManager BlobManagerObj = new BlobManager("photos");
+            BlobManagerObj.DeleteBlob(uri);
+            return RedirectToAction("Get");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }        
+    }
+}
---
+++ AzureBlobDemo/AzureBlobDemo/Services/BlobManager.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureBlobDemo.Services
+{
+    public class BlobManager
+    {
+        private CloudBlobContainer blobContainer;
+
+        public BlobManager(string ContainerName)
+        {
+            // Check if Container Name is null or empty  
+            if (string.IsNullOrEmpty(ContainerName))
+            {
+                throw new ArgumentNullException("ContainerName", "Container Name can't be empty");
+            }
+            try
+            {
+                // Get azure table storage connection string.  
+                string ConnectionString = "DefaultEndpointsProtocol=https;AccountName=demoazureblobstorage12;AccountKey=kmdBOAWZF08klxIjiOlpgv/HuNymM+zt2d1sw1YmwgOA0iF3187Cf+9o4jRr92MCDo1ynXPh8tYjg1sYCaxu9A==;EndpointSuffix=core.windows.net";
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+                CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+                blobContainer = cloudBlobClient.GetContainerReference(ContainerName);
+
+                // Create the container and set the permission  
+                if (blobContainer.CreateIfNotExists())
+                {
+                    blobContainer.SetPermissions(
+                        new BlobContainerPermissions
+                        {
+                            PublicAccess = BlobContainerPublicAccessType.Blob
+                        }
+                    );
+                }
+            }
+            catch (Exception ExceptionObj)
+            {
+                throw ExceptionObj;
+            }
+        }
+
+        public string UploadFile(IFormFile FileToUpload)
+        {
+            string AbsoluteUri;
+
+            // Check HttpPostedFileBase is null or not  
+            if (FileToUpload == null || FileToUpload.ContentDisposition.Length == 0)
+                return null;
+            try
+            {
+                string FileName = Path.GetFileName(FileToUpload.FileName);
+                CloudBlockBlob blockBlob;
+
+                // Create a block blob  
+                blockBlob = blobContainer.GetBlockBlobReference(FileName);
+                
+                // Set the object's content type  
+                blockBlob.Properties.ContentType = FileToUpload.ContentType;
+                
+                // upload to blob  
+                blockBlob.UploadFromStream(FileToUpload.OpenReadStream());
+
+                // get file uri  
+                AbsoluteUri = blockBlob.Uri.AbsoluteUri;
+            }
+            catch (Exception ExceptionObj)
+            {
+                throw ExceptionObj;
+            }
+            return AbsoluteUri;
+        }
+
+        public List<string> BlobList()
+        {
+            List<string> _blobList = new List<string>();
+            foreach (IListBlobItem item in blobContainer.ListBlobs())
+            {
+                if (item.GetType() == typeof(CloudBlockBlob))
+                {
+                    CloudBlockBlob _blobpage = (CloudBlockBlob)item;
+                    _blobList.Add(_blobpage.Uri.AbsoluteUri.ToString());
+                }
+            }
+            return _blobList;
+        }
+
+        public bool DeleteBlob(string AbsoluteUri)
+        {
+            try
+            {
+                Uri uriObj = new Uri(AbsoluteUri);
+                string BlobName = Path.GetFileName(uriObj.LocalPath);
+
+                // get block blob refarence  
+                CloudBlockBlob blockBlob = blobContainer.GetBlockBlobReference(BlobName);
+
+                // delete blob from container      
+                blockBlob.Delete();
+                return true;
+            }
+            catch (Exception ExceptionObj)
+            {
+                throw ExceptionObj;
+            }
+        }
+    }
+}
